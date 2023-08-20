bbcc9f0e05a71c00ddc108f1d98cfaed1c2495b6
TheJackal03 <gregorytsak3@gmail.com>
2023-08-20 21:44:29 +0300

    small changes

--- square_of_numbers.js
+++ square_of_numbers.js
@@ -51,6 +51,7 @@ console.log("Less complex squearing: " + less_complex_Squearing(originalArray))
     - We also use the .map command and again by using an arrow function as the argument inside that command, we calculate the square of the value in the array and then return everything automatically. 
     This happens because we didn't use curly brackets to open the arrow functions. If we did use the curly brackets we would have to use the return keyword.
 */
+
 const simpleSquearing = (arr) => arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x)
 
 console.log("Simplest way: " + simpleSquearing(originalArray))
