6c4825f4592c123fe25e6f6e800cf2d6cd9ded2c
TheJackal03 <gregorytsak3@gmail.com>
2023-08-21 20:30:51 +0300

    finished .reduce method script

--- reduce_method.js
+++ reduce_method.js
@@ -33,10 +33,10 @@ const items = [
     {name: "Monitor", price: 100}]
 
 //simple way:
-// let totalPrice = 0
-// items.forEach(item => {
-//     totalPrice += item.price
-// })
+let totalPrice = 0
+items.forEach(item => {
+    totalPrice += item.price
+})
 
 // console.log(totalPrice)
 
@@ -48,14 +48,14 @@ const items = [
  //   \  /
  //    \/
 
-const totalPrice = items.reduce((total, item) => {
+const totalPriceReduce = items.reduce((total, item) => {
     console.log(`Total: ${total}`)
     console.log(`Item: ${item.price} \n`)
 
     return total + item.price
 }, 0)
 
-console.log(totalPrice)
+console.log(totalPriceReduce)
 
 
 
@@ -89,3 +89,57 @@ const result = people.reduce((groupedPeople, person) => {
 
 console.log(result)
 
+
+
+
+
+/*                  ---------------------------------------------------------------
+                                            3rd Example 
+        - In this example we show that the function inside the .reduce method actually takes 4
+        parameters.
+        1st) is the parameter that is called the accumulator, which again, is the value that stores the second agrument of .reduce method, usually called starting point
+
+        2nd) is the parameter that is used to iterate over an array, meaning that it takes the place
+        for each item, similar to ==> (foreach item in array)
+
+        ** Usually these two are mostly used on the .reduce function
+
+        ---------------------------------------------------------------
+
+        3rd) is the paramter that is used to store/show (in this example) the position the function
+        is, every time it iterates over the array
+
+        4rd) is the parameter used to show on which array we're working on. It can either store the
+        name of the array or the number of values/position there are inside the array or even print the whole array everytime.
+*/
+
+const numbers = [13, 2, 5]
+
+const sum = numbers.reduce((total, number, index, array) => {
+    console.log(index, array)
+    return total + number
+}, 0)
+
+console.log(sum)
+
+/*                  ---------------------------------------------------------------
+                                            Keep in mind:
+        - That we don't have to use the second argument on the .reduce method, we can leave it empty 
+        like this:
+        ---------------------------------------------------------------
+                    const numbers = [13, 2, 5]
+
+                    const sum = numbers.reduce((total, number, index, array) => {
+                        console.log(index, array)
+                        return total + number
+                    })
+
+                    console.log(sum)
+        ---------------------------------------------------------------
+
+        and it will still run and return 20, which is correct in this case.
+
+        - What changes is that when we don't put a value, the starting point/value will be the first value on the first position of the array, which means incase we have an empty array, an error will be thrown back to us.
+
+        ** So it is advisable to NOT leave the second argument of the .reduce method blank.
+*/
