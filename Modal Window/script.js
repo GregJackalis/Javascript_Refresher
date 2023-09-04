04141649b26c68e94852dde35582a100c264d12c
TheJackal03 <gregorytsak3@gmail.com>
2023-09-04 12:58:21 +0300

    small changes on modal window

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -1,9 +1,8 @@
 "use strict";
 
 //buttons
-const BtnShowModal = document.querySelectorAll('.show-modal') // this is like a nodeList/Array that includes all the buttons with this class on our index.html
-const BtnCloseModal = document.querySelectorAll('.close-modal') //start with Btn so that it will be easier later in case we
-                                                            //we want to seaech for a button
+const BtnShowModal = document.querySelectorAll('.show-modal')
+const BtnCloseModal = document.querySelectorAll('.close-modal')
 const BtnSignUp = document.querySelector('#BtnSignUp')
 
 
@@ -22,11 +21,6 @@ const closeModal = function () {
     hiddenMessage.style.display = 'none';
     emailInput.value = '';
     passwordInput.value = '';
-
-     /* we can also use
-            hiddenModal.classList.add('hidden');
-            hiddenModal.classList.add('hidden');
-        */
 }
 
 // to open modal
@@ -34,11 +28,6 @@ Array.from(BtnShowModal).forEach(element => {
     element.addEventListener('click', () => {
         hiddenModal.style.display = 'block'; 
         overlayDisplay.style.display = 'block';
-
-        /* we can also use
-            hiddenModal.classList.remove('hidden');
-            hiddenModal.classList.remove(hidden');
-        */
     })
 }); {
 }
@@ -53,7 +42,6 @@ Array.from(BtnCloseModal).forEach(element => {
     })
 })
 
-// BtnCloseModal.addEventListener('click', closeModal)
 overlayDisplay.addEventListener('click', closeModal)
 
 BtnSignUp.addEventListener('click', () => {
@@ -62,19 +50,9 @@ BtnSignUp.addEventListener('click', () => {
     passwordInput.value = '';
 })
 
-document.addEventListener('keydown', (e) => { // e stands fo event, meaning the event that is occured when the .addEventListener function is called
-    //  console.log(e) // this will print information about the key we press, like an object, and then we will use the code of the key to refer to it and use it inside this .addEventListener 
+document.addEventListener('keydown', (e) => { 
 
-    // console.log(e.key)
-
-     if (e.key ==='Escape' && hiddenModal.style.display == 'none') { // or hiddenModal.classList.contains('hidden')
+     if (e.key ==='Escape' && hiddenModal.style.display == 'none') { 
         alert("Modal is already closed")
      } else if (e.key === 'Escape') closeModal();
 })
-
-// Keep in mind that if the othe method is used:
-//      hiddenModal.classList.remove('hidden')
-// then the same way we have to add the class and also check with the .contain method 
-
-// WE CANNOT combine both the method of changing the style in the css file and the method of removing
-// adding and checking w/ .contains the classes
